eec57197f229adbb6e456e365f8fe6dbbb82fda5
Gavin Franklin <60778485+Astronil@users.noreply.github.com>
2021-06-25 12:37:08 +0100

    fixed flip?

--- script.js
+++ script.js
@@ -46,10 +46,16 @@ function dropdown3desk() {
         }
       }
     }
-  }
-
-    // Close the dropdown if the user clicks outside of it
-    window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('flip')) {
+          openDropdown.classList.remove('flip');
+        }
+      }
+    }
       if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
